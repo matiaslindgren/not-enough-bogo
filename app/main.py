753cd0101c9fd9f8675eca037af0e7df2cf280b4
Matias Lindgren <matias.lindgren@gmail.com>
2017-01-17 22:06:48 +0200

    add celery task queue for doing the bogosorting in background

--- app/main.py
+++ app/main.py
@@ -1,8 +1,36 @@
 import flask
-app = flask.Flask(__name__)
+import tasks
 
-@app.route("/")
-def hi(name=None):
+BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+
+flask_app = flask.Flask(__name__)
+flask_app.config.update(
+    CELERY_BROKER_URL=BROKER_URL,
+    CELERY_RESULT_BACKEND=BROKER_URL
+)
+celery = tasks.make_celery(flask_app)
+
+
+@flask_app.route("/")
+def main(name=None):
     return flask.render_template('main.html', name=name)
 
+
+@celery.task()
+def sort_until_done(integers):
+    import time
+    time.sleep(1)
+    return sorted(integers)
+
+
+# def bogosort(seq):
+    # """In-place bogosort with inversion number counter."""
+    # inversion_counter = collections.Counter()
+    # current_inversions = inversions.inversion_number(seq)
+
+    # # bogosort
+    # while current_inversions:
+    #     inversion_counter[current_inversions] += 1
+    #     random.shuffle(seq)
+    #     current_inversions = inversions.inversion_number(seq)
 
---
+++ app/tasks.py
@@ -0,0 +1,22 @@
+import celery
+
+
+def make_celery(app):
+    selleri = celery.Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    selleri.conf.update(app.config)
+
+    TaskBase = selleri.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    selleri.Task = ContextTask
+
+    return selleri
