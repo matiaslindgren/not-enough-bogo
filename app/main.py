629b2d79eb24c84e5c0b630b70e5971d88f3532e
Matias Lindgren <matias.lindgren@gmail.com>
2017-01-17 23:01:54 +0200

    outline main module structure

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import flask
 import tasks
+import numpy
+
 
 BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 
@@ -16,21 +18,51 @@ def main(name=None):
     return flask.render_template('main.html', name=name)
 
 
+@flask_app.route("/history")
+def history():
+    # return flask.render_template('history.html', name=name)
+    return "TODO"
+
+
 @celery.task()
 def sort_until_done(integers):
     import time
-    time.sleep(1)
+    time.sleep(5)
     return sorted(integers)
 
 
-# def bogosort(seq):
-    # """In-place bogosort with inversion number counter."""
-    # inversion_counter = collections.Counter()
-    # current_inversions = inversions.inversion_number(seq)
+def get_from_db():
+    #TODO
+    return []
 
-    # # bogosort
-    # while current_inversions:
-    #     inversion_counter[current_inversions] += 1
-    #     random.shuffle(seq)
-    #     current_inversions = inversions.inversion_number(seq)
 
+def next_list(n):
+    return list(range(1, n+1))
+
+
+def powers_of_ten(start=1, end=7):
+    return map(lambda n: 10**n, range(start, end))
+
+
+def restart_from_previous_known_state():
+    sequence = get_from_db()
+    bogo(sequence)
+
+
+def bogo(sequence=None):
+    prev_ten_exponent = int(numpy.log10(len(sequence))) if sequence else 1
+    is_restarting = sequence is not None
+
+    for length in powers_of_ten(prev_ten_exponent, 7):
+        if is_restarting:
+            is_restarting = False
+        else:
+            sequence = next_list(length)
+        result = sort_until_done.delay(sequence)
+        result.wait()
+
+
+
+if __name__ == "__main__":
+    if not celery.current_task:
+        restart_from_previous_known_state()
