0de83bcf27dcd4858c6a6db41b91b445374edc1a
Matias Lindgren <matias.lindgren@gmail.com>
2017-01-19 23:28:24 +0200

    certainly not copy pasted...

--- app/tests/test.py
+++ app/tests/test.py
@@ -1,24 +1,24 @@
 import unittest
 import os
-import flaskr
 import tempfile
-import main
+import src.main as main
+
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        self.db_file_desc, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        with flaskr.app.app_context():
-            flaskr.init_db()
+        self.db_file_desc, main.flask_app.config['DATABASE'] = tempfile.mkstemp()
+        main.flask_app.config['TESTING'] = True
+        self.app = main.flask_app.test_client()
+        with main.flask_app.app_context():
+            main.init_db()
 
     def test_normalized_messiness(self):
         self.assertEqual(0, main.normalized_messiness([1]))
 
     def tearDown(self):
         os.close(self.db_file_desc)
-        os.unlink(flaskr.app.config['DATABASE'])
+        os.unlink(main.flask.app.config['DATABASE'])
 
 
 if __name__ == "__main__":
