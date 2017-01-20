8a72ed64108c12b55a43f761d4a7d2aaf88c2e81
Matias Lindgren <matias.lindgren@gmail.com>
2017-01-20 11:41:25 +0200

    increase test data sets

--- app/tests/test.py
+++ app/tests/test.py
@@ -3,6 +3,8 @@ import os
 import tempfile
 import src.main as main
 
+from hypothesis import strategies, given
+
 
 class Test(unittest.TestCase):
 
@@ -13,8 +15,21 @@ class Test(unittest.TestCase):
         with main.flask_app.app_context():
             main.init_db()
 
-    def test_normalized_messiness(self):
-        self.assertEqual(0, main.normalized_messiness([1]))
+    def test_normalized_messiness_sorted(self):
+        for xs in (list(range(1, n)) for n in range(2, 100)):
+            self.assertEqual(
+                0,
+                main.normalized_messiness(xs),
+                "Sorted lists should have messiness equal to 0"
+            )
+
+    def test_normalized_messiness_notsorted(self):
+        for xs in (list(reversed(range(1, n))) for n in range(2, 100)):
+            self.assertLess(
+                0,
+                main.normalized_messiness(xs),
+                "Reversed sorted lists should have messiness greater than 0"
+            )
 
     def tearDown(self):
         os.close(self.db_file_desc)
