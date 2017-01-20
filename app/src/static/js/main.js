f52f992ae53545c4f503fb1b9454197c7ba999dd
Matias Lindgren <matias.lindgren@gmail.com>
2017-01-20 22:23:48 +0200

    show iteration speed on the index page

--- app/src/static/js/main.js
+++ app/src/static/js/main.js
@@ -1,1 +1,14 @@
+$(function () {
 
+  function update_iteration_speed() {
+    $.ajax("current_speed.json")
+      .done(function(data) {
+        $("#iteration-speed").text(data['current_speed']);
+      });
+  }
+
+  while (true) {
+    setTimeout(update_iteration_speed, 100);
+  }
+
+});
