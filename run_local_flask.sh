b51c10669b00bf799462b50a40c0a91188311e93
Matias Lindgren <matias.lindgren@gmail.com>
2017-01-24 14:45:12 +0200

    update run script

--- run_local_flask.sh
+++ run_local_flask.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+echo
+echo Building js sources
+npm run build
+echo
+
+echo Running backend tests
+echo
 source $HOME/.virtualenvs/bogoenv/bin/activate
-export PYTHONPATH=$(pwd)/bogo
-export FLASK_APP=$(pwd)/bogo/main.py
+export FLASK_APP=bogo/bogo/main.py
+python3 -m unittest discover -s bogo -v
+echo
+
+echo
+if [[ $? != 0 ]]; then
+  echo Something went wrong
+  exit $?
+else
+  echo Everything seems to be fine
+fi
+echo
+echo
+
 python3 -m flask run
