8f4cea3088c6fc2cb55c0ac629d3e1f1b9c2eb28
Matias Lindgren <matias.lindgren@gmail.com>
2017-01-24 22:46:30 +0200

    enhance statistics table

--- bogo/static/jsx/ui.jsx
+++ bogo/static/jsx/ui.jsx
@@ -1,34 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 class Statistics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stateName:    "Loading...",
+      currentSpeed: "Loading..."
+    };
+  }
+
   componentDidMount() {
-    if (!this.state.endDate)
-      this.timerID = setInterval(_ => this.refreshState(), 1000);
+    // Statistics loaded for page with sorted sequence, don't refresh
+    if (this.state.stateName === "Sorted")
+      return;
+
+    this.timerID = setInterval(_ => this.refreshState(), 1000);
   }
 
   componentWillUnmount() {
-    if (this.timerID)
-      clearInterval(this.timerID);
+    clearInterval(this.timerID);
   }
 
   refreshState() {
-    // Retrieve url to statistics resource rendered by the backend
-    const JSONPath = JSON.parse($("#bogo-data-api").html())['bogoStatsUrl'];
-    // Update this state with backend state
-    $.getJSON(JSONPath, data => this.setState(data));
+    const jsonURL = this.props.jsonURL;
+    // Update own state with the current state of the backend
+    $.getJSON(jsonURL, data => {
+      const newState = Object.assign(
+        data,
+        (data.endDate) ?
+          { stateName: "Sorted", currentSpeed: "-" } :
+          { stateName: this.props.activeName, currentSpeed: Math.round(data.currentSpeed) + " shuffles per second" }
+      );
+      this.setState(newState);
+
+      if (data.endDate)
+        this.componentWillUnmount();
+    });
   }
 
   render() {
     return (
       <div>
-        <table className="table table-condensed">
+        <table className="table table-bordered table-condensed">
           <tbody>
-            <Row label="Sorting started"  value={this.state.startDate} />
-            <Row label="Sorting finished" value={this.state.endDate} />
-            <Row label="Sequence length"  value={this.state.sequenceLength} />
-            <Row label="Current speed"    value={Math.round(this.state.currentSpeed) + " shuffles per second"} />
+            <Row label="State"               value={this.state.stateName} />
+            <Row label="Sorting started at"  value={this.props.startDate} />
+            <Row label="Sorting finished at" value={this.state.endDate} />
+            <Row label="Sequence length"     value={this.props.sequenceLength} />
+            <Row label="Current speed"       value={this.state.currentSpeed} />
           </tbody>
         </table>
       </div>
@@ -47,7 +67,30 @@ function Row(props) {
 }
 
 
+function generateActiveName() {
+  const states = [
+    "with great enthusiasm",
+    "vigorously",
+    "with seemingly unlimited passion",
+    "rather impetuously",
+    "in an unreasoned manner",
+    "like a furious Jerboa",
+    "with passion",
+    "ironically fast",
+    "while occasionally sipping cheap red wine",
+    "furiously",
+    "with white shores and green fields in mind",
+  ];
+ return "Bogosorting " + states[Math.floor(Math.random()*states.length)];
+}
+
+
+const STATIC_DATA = JSON.parse($("#bogo-data-api").html());
+
 ReactDOM.render(
-  <Statistics />,
+  <Statistics jsonURL={STATIC_DATA["bogoStatsUrl"]}
+              startDate={STATIC_DATA['startDate']}
+              activeName={generateActiveName()}
+              sequenceLength={STATIC_DATA['sequenceLength']}/>,
   document.getElementById('react-root')
 );
