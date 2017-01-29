6b1e8417492356511fc127196fd9990a87beadb4
Matias Lindgren <matias.lindgren@gmail.com>
2017-01-30 00:13:24 +0200

    implement fullscreen for dummy sort page

--- bogo/static/js/dummySort.js
+++ bogo/static/js/dummySort.js
@@ -6,8 +6,39 @@ const animationSettings = {
   columns:         columns,
   sorted:          false,
   yPadding:        0,
-  spacing:         1,
-  backgroundAlpha: 60
+  spacing:         0.05,
+  backgroundAlpha: 50
 }
 const animationWrapper = new AnimationWrapper(animationSettings);
 const p5app = new p5(animationWrapper.p5sketch());
+
+function toggleFullScreen() {
+  var canvasElement = $("canvas")[0];
+
+  if (!document.mozFullScreen && !document.webkitFullScreen) {
+    if (canvasElement.mozRequestFullScreen) {
+      canvasElement.mozRequestFullScreen();
+    } else {
+      canvasElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+    // hacky hackerpants strikes again, couldn't get the fullscreenchange-events to work
+    setTimeout(_ => {
+      $("canvas").width($(window).width());
+      $("canvas").height($(window).height());
+    }, 100);
+  } else {
+    if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else {
+      document.webkitCancelFullScreen();
+    }
+  }
+
+  document.addEventListener("keydown", e => {
+    if (e.keyCode == 13) {
+      toggleFullScreen();
+    }
+  }, false);
+}
+
+
