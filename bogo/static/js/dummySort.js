a8cdc1f24628481ba451441c3eda7fd22827612c
Matias Lindgren <matias.lindgren@gmail.com>
2017-02-07 18:47:20 +0200

    add prompt before cpu intensive animations will be rendered

--- bogo/static/js/dummySort.js
+++ bogo/static/js/dummySort.js
@@ -1,44 +1,47 @@
-const backendData = JSON.parse($("#backend-render-context").html());
-const columns = backendData.columnCount ? parseInt(backendData.columnCount) : 10000;
+// function toggleFullScreen() {
+//   var canvasElement = $("canvas")[0];
 
-const animationSettings = {
-  containerId:     "dummy-sort-container",
-  columns:         columns,
-  sorted:          false,
-  yPadding:        0,
-  spacing:         0.05,
-  backgroundAlpha: 50
-}
-const animationWrapper = new AnimationWrapper(animationSettings);
-const p5app = new p5(animationWrapper.p5sketch());
+//   if (!document.mozFullScreen && !document.webkitFullScreen) {
+//     if (canvasElement.mozRequestFullScreen) {
+//       canvasElement.mozRequestFullScreen();
+//     } else {
+//       canvasElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+//     }
+//     // hacky hackerpants strikes again, couldn't get the fullscreenchange-events to work
+//     setTimeout(_ => {
+//       $("canvas").width($(window).width());
+//       $("canvas").height($(window).height());
+//     }, 100);
+//   } else {
+//     if (document.mozCancelFullScreen) {
+//       document.mozCancelFullScreen();
+//     } else {
+//       document.webkitCancelFullScreen();
+//     }
+//   }
 
-function toggleFullScreen() {
-  var canvasElement = $("canvas")[0];
+//   document.addEventListener("keydown", e => {
+//     if (e.keyCode == 13) {
+//       toggleFullScreen();
+//     }
+//   }, false);
+// }
 
-  if (!document.mozFullScreen && !document.webkitFullScreen) {
-    if (canvasElement.mozRequestFullScreen) {
-      canvasElement.mozRequestFullScreen();
-    } else {
-      canvasElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-    }
-    // hacky hackerpants strikes again, couldn't get the fullscreenchange-events to work
-    setTimeout(_ => {
-      $("canvas").width($(window).width());
-      $("canvas").height($(window).height());
-    }, 100);
-  } else {
-    if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else {
-      document.webkitCancelFullScreen();
-    }
+
+function createAnimation(button) {
+  document.getElementById("dummy-sort-container").removeChild(button.parentElement);
+
+  const backendData = JSON.parse($("#backend-render-context").html());
+  const columns = backendData.columnCount ? parseInt(backendData.columnCount) : 10000;
+
+  const animationSettings = {
+    containerId:     "dummy-sort-container",
+    columns:         columns,
+    sorted:          false,
+    yPadding:        0,
+    spacing:         0.05,
+    backgroundAlpha: 50
   }
-
-  document.addEventListener("keydown", e => {
-    if (e.keyCode == 13) {
-      toggleFullScreen();
-    }
-  }, false);
+  const animationWrapper = new AnimationWrapper(animationSettings);
+  const p5app = new p5(animationWrapper.p5sketch());
 }
-
-
