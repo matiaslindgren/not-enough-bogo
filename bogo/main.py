48fc8f0621e8a4004d2c8d276257d7c52300a155
Matias Lindgren <matias.lindgren@gmail.com>
2017-09-17 23:31:29 +0300

    implement routes for individual bogo data

--- bogo/main.py
+++ bogo/main.py
@@ -5,6 +5,7 @@ import logging
 import sanic
 
 from bogoapp import util
+from bogoapp import bogo
 
 
 logging_format = ("%(asctime)s %(process)d-%(levelname)s "
@@ -28,6 +29,22 @@ logger.debug("Created all globals")
 async def template_response(name, context=None):
     return sanic.response.html(await jinja_app.render(name, context))
 
+
+async def get_bogo_by_id_or_404(bogo_id):
+    bogo_row = await database.bogo_by_id(bogo_id)
+    if not bogo_row:
+        raise sanic.exceptions.abort(404)
+    return bogo.Bogo.from_database_row(bogo_row)
+
+async def adjacent_bogos(bogo_obj):
+    older, newer = await database.adjacent_bogos(bogo_obj)
+    return (bogo.Bogo.from_database_row(older) if older else None,
+            bogo.Bogo.from_database_row(newer) if newer else None)
+
+def url_for_bogo(bogo_id):
+    return app.url_for("view_bogo", bogo_id=bogo_id)
+
+
 @app.route("/")
 async def index(request):
     return await template_response("index.html")
@@ -35,6 +52,28 @@ async def index(request):
 @app.route("/about")
 async def about(request):
     return await template_response("about.html")
+
+@app.route("/bogo/<bogo_id:int>")
+async def view_bogo(request, bogo_id):
+    data_url = app.url_for("bogo_json", bogo_id=bogo_id)
+    render_context = {"bogo_id":  bogo_id,
+                      "data_url": data_url}
+    return await template_response('index.html', render_context)
+
+@app.route("/bogo/<bogo_id:int>.json")
+async def bogo_json(request, bogo_id):
+    bogo = await get_bogo_by_id_or_404(bogo_id)
+    stats = {
+        'links': {'self': url_for_bogo(bogo_id)},
+        'data': bogo.as_dict()
+    }
+    prev_bogo, next_bogo = await adjacent_bogos(bogo)
+    if prev_bogo:
+        stats['links']['previous'] = url_for_bogo(prev_bogo.db_id)
+    if next_bogo:
+        stats['links']['next'] = url_for_bogo(next_bogo.db_id)
+    return sanic.response.json(stats)
+
 
 @app.listener("before_server_start")
 async def begin_sort(app, loop):
